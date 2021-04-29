a10899d03637ea15e5b8b5e7afccd41a947bdd15
Maikol Moreira <Niukeitor@hotmail.com>
2021-04-29 16:34:41 +0000

    boiler

---
+++ src/js/component/counter.js
@@ -0,0 +1,8 @@
+import React from "react";
+export function Counter () {
+    return (
+        <div className="bg-dark border border-light m-1 rounded text-center text-light">
+            <h1>3</h1>
+        </div>
+    );
+}
